e15f34f7ad5fef1846880c89fe603d5f7c1f2d3e
mayolar221 <mayolar221@gmail.com>
2024-07-29 23:24:20 +0900

    16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Mir Acme Dashboard",
+  },
+  description:
+    "The official Next.js Learn Dashboard built with App Router by Mir.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({
   children,
